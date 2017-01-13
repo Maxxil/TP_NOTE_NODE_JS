76d8c74f6e3d2473453b51575d9adf87cbf32169
Maxxil <massil.kadi@ynov.com>
2017-01-13 14:37:38 +0100

    ajout home controller et modification index controller / modification emplacement bootstrap

--- controller/home-controller.js
+++ controller/home-controller.js
@@ -3,6 +3,8 @@
  */
 var router = require("express").Router();
 
-router.use("/" , require("./home-controller"));
+router.get("/" , function(req , res){
+    res.render("./../views/index.html");
+});
 
 module.exports = router;
